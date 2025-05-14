1fb8690dae9f18eef47a2dd021aeda18795e8f55
xjh <xiaojianhuo2025@outlook.com>
2025-05-14 15:00:09 +0800

    remove quotes

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx http_proxy "http://127.0.0.1:55555"
+set -gx http_proxy http://127.0.0.1:55555
 set -gx https_proxy = $http_proxy
 
-set -gx VISUAL "nvim"
+set -gx GCM_CREDENTIAL_STORE cache
+
+set -gx VISUAL nvim
 set -gx EDITOR $VISUAL
+
