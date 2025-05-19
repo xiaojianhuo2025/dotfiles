ea04e0a11eb0d25e4a877f7c4329668d92494b53
xjh <xiaojianhuo2025@outlook.com>
2025-05-19 10:19:45 +0800

    change port

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx http_proxy http://127.0.0.1:55555
+set -gx http_proxy http://127.0.0.1:12334
 set -gx https_proxy $http_proxy
 
 set -gx GCM_CREDENTIAL_STORE cache
