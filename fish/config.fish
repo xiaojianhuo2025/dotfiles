cf6839f9bdf8d27155be996c526bf507570b4618
xjh <xiaojianhuo2025@outlook.com>
2025-11-21 22:57:17 +0800

    add fish alias and use thefuck

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,12 @@ set -gx GCM_CREDENTIAL_STORE cache
 set -gx KWIN_USE_OVERLAYS 1
 set -gx VISUAL nvim
 set -gx EDITOR $VISUAL
-set -gx AVALONIA_GLOBAL_SCALE_FACTOR 2
+# set -gx AVALONIA_GLOBAL_SCALE_FACTOR 2
 set -gx BAT_THEME ansi
 
 alias ns="gamemoderun ryujinx"
+alias psp="gamemoderun ppsspp"
+alias ps2="gamemoderun pcsx2"
+alias ff="fastfetch -l none"
+
+thefuck --alias | source
