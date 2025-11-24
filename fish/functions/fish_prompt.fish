3dbd9f99e812ec5e717efa175a725315f7b77911
xjh <xiaojianhuo2025@outlook.com>
2025-11-24 22:57:53 +0800

    use existing prompt theme

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,31 +1,20 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-    set -l normal (set_color normal)
-    set -q fish_color_status
-    or set -g fish_color_status red
-
-    # Color the prompt differently when we're root
-    set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
-    if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
+function fish_prompt --description 'Informative prompt'
+        #Save the return status of the previous command
+        set -l last_pipestatus $pipestatus
+        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    
+        if functions -q fish_is_root_user; and fish_is_root_user
+                printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                                     and set_color $fish_color_cwd_root
+                                                                     or set_color $fish_color_cwd) \
+                        (prompt_pwd) (set_color normal)
+        else
+                set -l status_color (set_color $fish_color_status)
+                set -l statusb_color (set_color --bold $fish_color_status)
+                set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+        
+                printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
+                        $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+                        (set_color normal)
         end
-        set suffix '#'
-    end
-
-    # Write pipestatus
-    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-    set -l bold_flag --bold
-    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-    if test $__fish_prompt_status_generation = $status_generation
-        set bold_flag
-    end
-    set __fish_prompt_status_generation $status_generation
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color $bold_flag $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-    echo -n -s (date +%H:%M) ' ' (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status $suffix ' '
 end
